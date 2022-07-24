1ca6508562d74b7b579008a9581e83900ab8b4a5
Emerson Millard <emillard.code@gmail.com>
2022-07-24 12:00:00 -0700

    Moved controller methods to controller class.

--- src/main/java/com/project/controller/ChatbookController.java
+++ src/main/java/com/project/controller/ChatbookController.java
@@ -1,5 +1,7 @@
 package com.project.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/chatbook-application")
 public class ChatbookController {
 
+    // This gets the "server.port" value from the application.properties file.
+    @Value("${server.port}")
+    private String port;
 
+    @GetMapping("/chat")
+    public String chatNow() {
+
+        return "application is up on port : " + port;
+
+    }
+
+    @GetMapping("/")
+    public String refresh() {
+        return "";
+    }
 
 }

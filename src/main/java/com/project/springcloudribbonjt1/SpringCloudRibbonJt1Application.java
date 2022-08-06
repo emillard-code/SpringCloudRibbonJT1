35b4acee8116437fd45ce702f5fce9ce7f9a8c34
Emerson Millard <emillard.code@gmail.com>
2022-08-06 12:00:00 -0700

    Added controller methods to main class.

--- src/main/java/com/project/springcloudribbonjt1/SpringCloudRibbonJt1Application.java
+++ src/main/java/com/project/springcloudribbonjt1/SpringCloudRibbonJt1Application.java
@@ -1,13 +1,31 @@
 package com.project.springcloudribbonjt1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/chatbook-application")
 public class SpringCloudRibbonJt1Application {
 
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/chat")
+    public String chatNow() {
+
+        return "application is up on port : " + port;
+        
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCloudRibbonJt1Application.class, args);
+
     }
 
 }

6e9d60841526c8ae7d9b3e1fb0e80b6904cfd466
Emerson Millard <emillard.code@gmail.com>
2022-08-10 19:20:43 -0700

    Initial commit

---
+++ src/main/java/com/project/springcloudribbonjt1/SpringCloudRibbonJt1Application.java
@@ -0,0 +1,13 @@
+package com.project.springcloudribbonjt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRibbonJt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudRibbonJt1Application.class, args);
+    }
+
+}

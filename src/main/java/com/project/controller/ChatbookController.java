8a00f1c62b1c6c7e9f0b7be52b4b0a002ea54d78
Emerson Millard <emillard.code@gmail.com>
2022-08-11 01:03:11 -0700

    Added controller class.

---
+++ src/main/java/com/project/controller/ChatbookController.java
@@ -0,0 +1,12 @@
+package com.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chatbook-application")
+public class ChatbookController {
+
+
+
+}
